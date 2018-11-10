9802488738668bedf3c8cb46ffacd35c98ad2593
ilia509 <theilia509@gmail.com>
2018-11-10 15:39:35 +0300

    start

---
+++ main.py
@@ -0,0 +1,10 @@
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+import config
+
+vk_session = vk_api.VkApi(config.login, config.token)
+vk_session.auth(token_only=True)
+
+vk = vk_session.get_api()
+
+longpoll = VkLongPoll(vk_session)
