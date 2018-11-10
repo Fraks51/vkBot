71a39c38e9deee235a2cf409918ad1c2c2b33c12
ilia509 <theilia509@gmail.com>
2018-11-10 16:09:47 +0300

    password added

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 import config
 
-vk_session = vk_api.VkApi(config.login, config.token)
+vk_session = vk_api.VkApi(config.login, config.password,config.token)
 vk_session.auth(token_only=True)
-
 vk = vk_session.get_api()
 
 longpoll = VkLongPoll(vk_session)
+
+for event in longpoll.listen():
+    print(event.text)
