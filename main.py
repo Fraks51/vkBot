2958ca98bc1d7e50dbc9a90067adfa3cfc1041f6
ilia509 <theilia509@gmail.com>
2018-11-10 18:13:09 +0300

    attachments

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 import config
 
-vk_session = vk_api.VkApi(config.login, config.password,config.token)
+vk_session = vk_api.VkApi(config.login, config.password, config.token)
 vk_session.auth(token_only=True)
 vk = vk_session.get_api()
 
 longpoll = VkLongPoll(vk_session)
 
+
+
 for event in longpoll.listen():
-    print(event.text)
+    if(event.type == VkEventType.MESSAGE_NEW):
+        print(event)
+        print(event.text)
+        # print(event.payload)
+        print(event.attachments)
